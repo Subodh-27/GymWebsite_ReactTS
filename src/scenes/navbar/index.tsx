e8d8a668e573b531f4b2175a81edbdbf344a619f
Subodh-27 <sbhawt@gmail.com>
2024-02-10 01:45:04 +0530

    Mobile Menu Button Added with Functionality

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -63,9 +63,8 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                             ) 
                             : (
                                 <button
-                                    className="rounded-full bg-secondary-500 p-2
-                                        onClick={() => setIsMenuToggled(!isMenuToggled)}
-                                    "
+                                    className="rounded-full bg-secondary-500 p-2"
+                                    onClick={ () => setIsMenuToggled(!isMenuToggled) }
                                 >
                                     <Bars3Icon className="h-6 w-6 text-white" />
                                 </button>
@@ -73,6 +72,42 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
                     </div>
                 </div>
             </div>
+
+            {/* Mobile Menu Modal */}
+            { !isAboveMediumScreens && isMenuToggled && (
+                <div className="fixed right-0 bottom-0 z-40 h-full w-[500px] bg-primary-100 drop-shadow-xl">
+                    {/* Close Icon */}
+                    <div className="flex justify-end p-12">
+                        <button onClick={ () => setIsMenuToggled(!isMenuToggled) }>
+                            <XMarkIcon className="h-6 w-6 text-gray-400" />
+                        </button>
+                    </div>
+
+                    {/* Menu Items */}
+                    <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                        <Link 
+                            page="Home" 
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link 
+                            page="Benefits" 
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link 
+                            page="Our Classes" 
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                        <Link 
+                            page="Contact Us" 
+                            selectedPage={selectedPage}
+                            setSelectedPage={setSelectedPage}
+                        />
+                    </div>
+                </div>
+            )}
         </nav>
     )
 }
