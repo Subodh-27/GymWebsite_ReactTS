c35a37a8f88205931e3c6a19109e23f59b14338a
Subodh-27 <sbhawt@gmail.com>
2024-02-14 01:07:14 +0530

    Benefits Page Animation

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -23,6 +23,13 @@ const benefits: Array<BenefitType> = [
     }
 ]
 
+const container = {
+    hidden: {},
+    visible: {
+        transition: { staggerChildren: 0.2 }
+    }
+}
+
 type Props = {
     setSelectedPage: (value: SelectedPage) => void;
 }
@@ -37,15 +44,31 @@ const Benefits = ({ setSelectedPage }: Props) => {
             onViewportEnter={ () => setSelectedPage(SelectedPage.Benefits) }
         >
             {/* Header */}
-            <div className="md:my-5 md:w-3/5">
+            <motion.div 
+                className="md:my-5 md:w-3/5"
+                initial= "hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                transition={{ duration: 1 }}
+                variants={{
+                    hidden: { opacity: 0, x: -50 },
+                    visible: { opacity: 1, x: 0 }
+                }}
+            >
                 <HText>MORE THAN JUST A GYM.</HText>
                 <p className="my-5 text-sm">
                     We provide world class fitness equipment, trainers and classes to get you to your ultimate fitness goals with ease. We provide true care into each and every member.
                 </p>
-            </div>
+            </motion.div>
 
             {/* Benefits */}
-            <div className="md:flex items-center justify-between gap-8 mt-5">
+            <motion.div 
+                className="md:flex items-center justify-between gap-8 mt-5"
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true, amount: 0.5 }}
+                variants={ container }
+            >
                 { benefits.map((benefit: BenefitType) => (
                     <Benefit 
                         key={benefit.title}
@@ -55,7 +78,7 @@ const Benefits = ({ setSelectedPage }: Props) => {
                         setSelectedPage={setSelectedPage}
                     />
                 )) }
-            </div>
+            </motion.div>
         </motion.div>
     </section>
   )
