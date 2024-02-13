8e17f5f155071b3fcfe51d2d764dbf22c1d26c84
Subodh-27 <sbhawt@gmail.com>
2024-02-13 17:58:43 +0530

    Home Page Completed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 import Navbar from "@/scenes/navbar"
 import { SelectedPage } from "@/shared/types"
+import Home from '@/scenes/home'
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   )
 }
