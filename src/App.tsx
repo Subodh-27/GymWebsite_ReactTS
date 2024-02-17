a26c52b7617404c54513b3f77660afc6a2bf2ca2
Subodh-27 <sbhawt@gmail.com>
2024-02-17 23:58:33 +0530

    Our Classes Started

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 
-import Navbar from "@/scenes/navbar"
 import { SelectedPage } from "@/shared/types"
+import Navbar from "@/scenes/navbar"
 import Home from '@/scenes/home'
 import Benefits from '@/scenes/benefits'
+import OurClasses from '@/scenes/ourClasses'
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
@@ -31,6 +32,7 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   )
 }
